4f6c08f9ae334a6fa00ce6ac7e329c6e6ba3e7cf
Jayakumar Vinayagam <jayakumarvinayagam008@gmail.com>
2018-07-28 13:55:39 +0530

    solution created and added.

---
+++ CustomerDataProcess/CustomerDataProcess/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomerDataProcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
